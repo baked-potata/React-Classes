b2926f40f8f5ccf522c56df98e8c3bd99cd36a9a
rahul <baked-potata@protonmail.com>
2022-11-22 16:25:55 +0530

    css updates

--- first-app/src/components/Header.jsx
+++ first-app/src/components/Header.jsx
@@ -1,6 +1,17 @@
 import "./Header.css";
 
 function Header() {
+  const styleElement = {
+    display: "flex",
+    fontSize: 56,
+    lineSpacing: 5,
+    //Notice how the properties with multiple words in name are converted into
+    //camelCase objects.
+    color: "#ae2356",
+    // The color code is also in double quotes as its a string.
+    letterSpacing: 18,
+  };
+
   return (
     <div className="nav">
       <ul>
@@ -10,6 +21,8 @@ function Header() {
         <li>d</li>
         <li>e</li>
       </ul>
+
+      <h1 style={styleElement}>This is some text here</h1>
     </div>
   );
 }
