1d77eb89448f91f5f5589634c68de336d4e35454
Rahul <baked-potata@protonmail.com>
2022-11-28 10:43:55 +0530

    React Routes

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -4,13 +4,31 @@ import Counter from "./components/Counter";
 import CounterWithObject from "./components/CounterWithObject";
 import Data from "./components/Data";
 import { useState } from "react";
+import Home from "./pages/Home";
+import Navigation from "./components/Navigation";
+import About from './pages/About'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Services from "./pages/Services";
+import Blog from './pages/Blog'
+import Contact from "./pages/Contact";
 function App() {
   const [title, setTitle] = useState("some title");
   const handleClick = () => {
     setTitle("some changed title");
   };
+  const item=true;
+
+const conditonalRender = () =>
+{
+if (item)
+return <h1>some div </h1>
+
+}
+
   return (
     <>
+
+{/* {conditonalRender()} */}
       {/* <Header />
       <h1> kuch bhi</h1>
       <p> dusra kuch bhi</p>
@@ -28,10 +46,28 @@ function App() {
       <Counter />
 
       <CounterWithObject /> */}
-      <h3>{title} </h3>
+      {/* <h3>{title} </h3>
 
-      <Data ct={handleClick} />
-    </>
+    <Data ct={handleClick} /> */}
+    <BrowserRouter>
+    <Navigation />
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+
+      <Route path='/About' element={<About />}></Route>
+      <Route path="/Services" element={<Services />} ></Route>
+
+      <Route path="/Blog" element={<Blog />} ></Route>
+      <Route path="/Contact" element={<Contact />} ></Route>
+
+    </Routes>
+    </BrowserRouter>
+
+
+    </> 
+
+
+
   );
 }
 
